826ebceb23affef895836e837352439cac61bb50
Stanislav Minko <minkostas@gmail.com>
2024-10-15 01:47:35 +0300

    refactor

--- src/basics.js
+++ src/basics.js
@@ -3,3 +3,80 @@ import { pipeline } from 'node:stream';
 import { stdout } from 'node:process';
 import path from 'node:path';
 import fs from 'node:fs';
+
+
+
+export const cat = async (prop, currentFolder) => {
+    try {
+      const filePath = buildPath(prop, currentFolder);
+      await access(filePath, constants.R_OK);
+      const rs = fs.createReadStream(filePath);
+      rs.on('data', (chunk) => {
+        stdout.write(chunk);
+      });
+      rs.on('end', () => {
+        console.log(`\nYou are currently in ${currentFolder}`);
+      });
+    } catch (err) {
+      console.error(err.message);
+      console.log(`\nYou are currently in ${currentFolder}`);
+    }
+  };
+  export const add = async (prop, currentFolder) => {
+    try {
+      const filePath = buildPath(prop, currentFolder);
+      await open(filePath, 'wx');
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+  export const rn = async (props, currentFolder) => {
+    try {
+      if (props.length !== 2)
+        throw new Error('Need two arguments: path_to_file new_filename');
+      const newFile = path.basename(props[1]);
+      await access(props[0]);
+      rename(props[0], newFile);
+    } catch (err) {
+      console.log(err.message);
+    } finally {
+      console.log(`\nYou are currently in ${currentFolder}`);
+    }
+  };
+  export const cp = async (props, currentFolder) => {
+    try {
+      if (props.length !== 2)
+        throw new Error('Need two arguments: path_to_file path_to_new_directory');
+      await access(props[0]);
+      await access(props[1]);
+      const destination = path.join(props[1], path.basename(props[0]));
+  
+      pipeline(
+        fs.createReadStream(props[0]),
+        fs.createWriteStream(destination),
+        (err) => {
+          if (err) console.log(err?.message);
+        }
+      );
+      console.log('File copied...');
+    } catch (err) {
+      console.error(err.message);
+    } finally {
+      console.log(`\nYou are currently in ${currentFolder}`);
+    }
+  };
+  export const mv = async (prop) => {
+    console.log('mv');
+  };
+  export const rm = async (prop) => {
+    console.log('rm');
+  };
+  
+  const buildPath = (argPath, currentFolder) => {
+    let folder = argPath;
+    if (!path.isAbsolute(argPath)) {
+      folder = path.join(currentFolder, argPath);
+    }
+    return folder;
+  };
+  
--- src/index.js
+++ src/index.js
@@ -1,31 +1,74 @@
+import { systemInfo } from './system.js';
+import { cd } from './cd.js';
+import { ls } from './ls.js';
+import { goUp } from './up.js';
+import { cat, add, rn, cp, mv, rm } from './basics.js';
 import readline from 'readline';
-import os from 'os';
-import handleInputModule from '../input/handleInput.js';
-const { handleInput } = handleInputModule;
-import utils from '../utils/utils.js';
-process.chdir(os.homedir());
 
+const args = process.argv.slice(2);
+let currentFolder = process.cwd();
 
-let username = process.argv.find(arg => arg.startsWith('--username='))?.split('=')[1];
-if(username===undefined){
-  username = '%anonimus%';
-} 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+const fileManager = async () => {
+  args.forEach((val) => {
+    if (val.includes('username')) {
+      const string = val.split('=')[1];
+      console.log(
+        `Welcome to the File Manager, ${string ? string : '%anonimus%'}!`
+      );
+    }
+  });
+  console.log(`\nYou are currently in ${currentFolder}`);
 
-utils.printWelcomeMessage(username);
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
 
-rl.on('line', (input) => {
-    handleInput(input, rl);
-    rl.prompt();
-});
+  rl.on('line', async (line) => {
+    const [command, ...args] = line
+      .trim()
+      .split(' ')
+      .filter((val) => val !== '');
+    switch (command) {
+      case 'os':
+        systemInfo(args);
+        break;
+      case 'up':
+        currentFolder = goUp(currentFolder);
+        console.log(`\nYou are currently in ${currentFolder}`);
+        break;
+      case 'cd':
+        currentFolder = await cd(args[0], currentFolder);
+        console.log(`\nYou are currently in ${currentFolder}`);
+        break;
+      case 'ls':
+        await ls(currentFolder);
+        break;
+      case 'cat':
+        await cat(args[0], currentFolder);
+        break;
+      case 'add':
+        await add(args[0], currentFolder);
+        break;
+      case 'rn':
+        await rn(args, currentFolder);
+        break;
+      case 'cp':
+        await cp(args, currentFolder);
+        break;
+      case 'mv':
+        await mv(mv);
+        break;
+      case 'rm':
+        await rm(rm);
+        break;
+      case '.exit':
+        rl.close();
+        break;
+      default:
+        console.log('Please enter valid command...');
+    }
+  });
+};
 
-rl.on('close', () => {
-  utils.printGoodbyeMessage(username);
-  process.exit(0);
-});
-
-rl.prompt();
-
+fileManager();
